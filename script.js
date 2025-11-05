c6ea6b950b47d512eeccf9e5df81e8ff2b675700
Webwhizsofwan <yusufsofwan49@gmail.com>
2025-11-05 22:36:55 +0100

    change of js

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 const favoriteIcon = document.querySelectorAll(".favorite-icon");
 
-
-  favoriteIcon.forEach(icon => {
-    icon.addEventListener("click", () => {
-if (icon.innerHTML === "♡") {
-    icon.textContent = "❤️";
-    icon.classList.add(".filled");
-  }
-  else {
-    icon.textContent = "♡";
-    icon.classList.remove(".filled");
-  }
+favoriteIcon.forEach(icon => {
+  icon.addEventListener("click", () => {
+    if (icon.classList.contains("filled")) {
+      icon.innerHTML = "&#9825;";
+      icon.classList.remove("filled");
+    } else {
+      icon.innerHTML = "&#10084;";
+      icon.classList.add("filled");
+    }
   });
-  
-})
+});
